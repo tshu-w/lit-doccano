86de383f589853d80720dd26ea631f57fe3dfa12
Robert S Lee <sangkyulee@gmail.com>
2022-09-06 17:13:08 -0400

    add nginx.conf inside the code

--- lit_doccano/component.py
+++ lit_doccano/component.py
@@ -9,6 +9,22 @@ import lightning as L
 # dir where doccano python venv will be setup
 doccano_venv        = "venv-doccano"
 
+# remove x-frame-options
+nginx_conf = """
+events {
+  worker_connections 1024;
+}
+http {
+    server {
+      listen $port;
+      location / {
+        proxy_pass http://$host:$internal_port;
+        proxy_hide_header x-frame-options;
+      }
+  }
+}
+"""
+
 class DoccanoBuildConfig(L.BuildConfig):
   def build_commands(self) -> List[str]:
     return [
@@ -24,11 +40,9 @@ class LitDoccano(L.LightningWork):
 
     def run(self):
         # prepare nginx conf with host and port numbers filled in
-        conf_file = os.path.join(Path(__file__).parent.absolute(), "nginx.conf")
-        new_conf_file = os.path.join(Path(__file__).parent.absolute(), "nginx-new.conf")
+        new_conf_file = os.path.join(os. getcwd(), "nginx-new.conf")
         new_conf = open(new_conf_file, "w")
-        print(f"{conf_file}")
-        for l in open(conf_file).readlines():
+        for l in nginx_conf.splitlines():
             print(l)
             new_conf.write(Template(l).substitute(host='0.0.0.0', port=self.port, internal_port=8000))
         new_conf.close()
